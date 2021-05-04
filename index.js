5ac88f5cbf87684963627d2d214bb1f59d40e531
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-04 22:42:39 +1000

    started JS. I have added entryForm variable to ensure that the entry form doesn't reset whenever I click 'submit'

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// javascript
+/*
+ - Create a variable named "entryForm" and use the document.getElementById method
+   to make its value the entryForm from our HTML document.
+*/
+
+const entryForm = document.getElementById('entryForm');
+
+/*
+ - Add an event listener to the entryForm variable that prevents the page from
+   reloading whenever the submit button is clicked.
+*/
+
+function addEntryToDom(event) {
+    event.preventDefault();
+}
+
+entryForm.addEventListener('submit', addEntryToDom)
