904d2e5eb6c334755b5b8b4f479c00b64c11d0bb
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-05 22:07:47 +1000

    in this part, I screated a new section in HTML, manipulated that section by appending a child to it which was the innertext of the input of the text entry

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-/*
- - Create a variable named "entryForm" and use the document.getElementById method
-   to make its value the entryForm from our HTML document.
-*/
-
 const entryForm = document.getElementById('entryForm');
 const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementByClass('entry-textbox');
+const entryTextbox = document.getElementsByClassName('entry-textbox');
 
 /*
  - Add an event listener to the entryForm variable that prevents the page from
    reloading whenever the submit button is clicked.
 */
-
 function addEntryToDom(event) {
-    event.preventDefault();
+  event.preventDefault();
+  const entryDiv = document.createElement('div');
+  entryDiv.className = 'single-entry';
+  entryDiv.innerText = entryTextbox[0].value;
+  entriesSection.appendChild(entryDiv);
 }
 
-entryForm.addEventListener('submit', addEntryToDom)
+entryForm.addEventListener('submit', addEntryToDom);
