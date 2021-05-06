d3d369a0a217545f018007b6419ff3bfdcb8be73
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-06 22:13:10 +1000

    added const displayEntryButton = document.createElement('button')

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const entryForm = document.getElementById('entryForm');
-const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
 
 /*
  - Add an event listener to the entryForm variable that prevents the page from
@@ -10,8 +11,15 @@ function addEntryToDom(event) {
   event.preventDefault();
   const entryDiv = document.createElement('div');
   entryDiv.className = 'single-entry';
-  entryDiv.innerText = entryTextbox[0].value;
+  entryDiv.innerText = entryTextbox.value;
+  entryDiv.style.display = 'none';
   entriesSection.appendChild(entryDiv);
+  entryTextbox.value = '';
+
+  const displayEntryButton = document.createElement('button')
+  displayEntryButton.className = 'display-entry-button';
+  displayEntryButton.innerText = 1;
+  entriesNav.appendChild(displayEntryButton);
 }
 
 entryForm.addEventListener('submit', addEntryToDom);
